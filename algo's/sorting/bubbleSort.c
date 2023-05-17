6f70e16ae9b82226f7e8da22b559cbd974ea7911
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-05-17 20:11:08 +0530

    OK OK

---
+++ algo's/sorting/bubbleSort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void bubbleSort(int array[], int size){
+    int i, j;
+    for (i = 0; i < size - 1; i++){
+
+        for (j = 0; j < size - i - 1; j++){
+
+            if (array[j] > array[j + 1]){
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int array[], int size){
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int array[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(array) / sizeof(array[0]);
+
+    printf("Original array: ");
+    printArray(array, size);
+
+    bubbleSort(array, size);
+
+    printf("Sorted array: ");
+    printArray(array, size);
+
+    return 0;
+}
